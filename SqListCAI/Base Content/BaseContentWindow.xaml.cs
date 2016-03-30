4be1737f9884ab90ac1c8d01653b352b210059e9
JackRoderick <1021924817@qq.com>
2016-03-30 18:41:33 +0800

    first commit

---
+++ SqListCAI/Base Content/BaseContentWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SqListCAI.Utils;
+
+namespace SqListCAI.Base_Content
+{
+    /// <summary>
+    /// BaseContentWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class BaseContentWindow : Window
+    {
+        private TextBox textBox;
+        private string SQLIST_DEFINE = "";
+        private string SQLIST_FEATURE = "";
+        private string SQLIST_STRUCTURE = "";
+        private string SQLIST_OPERATOR = "";
+        public BaseContentWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            textBox = (TextBox)FindName("tb_showContent");
+        }
+
+        private void sqListDefine(object sender, RoutedEventArgs e)
+        {
+            SQLIST_DEFINE = System.Text.Encoding.Default.GetString(Files.read("sqListDefine.txt"));
+            textBox.Text = SQLIST_DEFINE;
+        }
+
+        private void sqListFeature(object sender, RoutedEventArgs e)
+        {
+            SQLIST_FEATURE = System.Text.Encoding.Default.GetString(Files.read("sqListFeature.txt"));
+            textBox.Text = SQLIST_FEATURE;
+        }
+
+        private void sqListStructure(object sender, RoutedEventArgs e)
+        {
+            SQLIST_STRUCTURE = System.Text.Encoding.Default.GetString(Files.read("sqListStructure.txt"));
+            textBox.Text = SQLIST_STRUCTURE;
+        }
+
+        private void sqListOperator(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SqListCAI/Utils/Files.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace SqListCAI.Utils
+{
+    class Files
+    {
+        public static byte[]  read(string fileName)
+        {
+            byte[] data = null;
+            try
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+                //toWhere = sr.ReadToEnd();//读取完整的文件，如果用这个方法，就可以不用下面的while循环
+                data = new byte[fs.Length];
+                fs.Read(data, 0, data.Length);
+                int offset = 0;
+                int remaining = data.Length;
+                // C#读取文件之只要有剩余的字节就不停的读  
+                while (remaining > 0)
+                {
+                    int read = fs.Read(data, offset, remaining);
+                    if (read <= 0)
+                        throw new EndOfStreamException("文件读取到" + read.ToString() + "失败！");
+                    // C#读取文件之减少剩余的字节数  
+                    remaining -= read;
+                    // C#读取文件之增加偏移量  
+                    offset += read;
+                }
+                Console.WriteLine(data + "123");
+                fs.Close();
+                return data;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("an IOexception has been thrown!");
+            }
+            return data;
+        }
+    }
+}
